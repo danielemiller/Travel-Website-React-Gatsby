4da3de62e02ddc15943ce249f710ddb14a321a67
Daniel Miller <danielemiller@live.com>
2020-12-21 11:43:54 -0500

    Edit package.json and gatsby.config for deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: `react-gatsby-travel`
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
